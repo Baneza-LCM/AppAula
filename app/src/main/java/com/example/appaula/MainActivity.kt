e2c02786abc648c2570b424225ff651a1b0f19e5
Baneza-LCM <baneza68liv@gmail.com>
2024-04-19 18:09:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appaula/MainActivity.kt
@@ -0,0 +1,155 @@
+package com.example.appaula
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appaula.ui.theme.AppAulaTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppAulaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App(){
+    Column(
+        Modifier
+            .background(Color.White)
+    ){
+        Row(
+            Modifier
+                .background(Color.White)
+                .fillMaxWidth(),
+            Arrangement.Center
+        ){
+            Text(
+                text = "App Aula",
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp
+            )
+
+        }
+        Row(
+            Modifier
+                .padding(8.dp),
+            Arrangement.SpaceBetween
+        ) {
+
+            Column(
+
+            ) {
+                Text(
+                    text = "Baneza Livia",
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 15.sp
+                )
+            }
+            Column(
+
+            ) {
+                Button(
+                    onClick = { /*TODO*/ }
+                ) {
+                  Text(text = "Editar")
+                }
+            }
+            Column(
+
+            ) {
+                Button(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(text = "Excluir")
+                }
+
+
+            }
+
+
+        }
+
+        Row(
+            Modifier
+                .padding(8.dp),
+            Arrangement.SpaceBetween
+        ) {
+
+            Column(
+
+            ) {
+                Text(
+                    text = "Baneza Livia",
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 15.sp
+                )
+            }
+            Column(
+
+            ) {
+                Button(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(text = "Editar")
+                }
+            }
+            Column(
+
+            ) {
+                Button(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(text = "Excluir")
+                }
+
+            }
+
+        }
+    }
+}
+
+@Composable
+@Preview
+fun AppPreview(){
+    AppAulaTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            App()
+        }
+    }
+}
